4953843adea5544dd5d7ca679ade8b49a10d3359
lidian <lidianmanoha@gmail.com>
2020-05-23 17:55:14 +0200

    config scss

--- client_dashboard/src/Homepage.component.jsx
+++ client_dashboard/src/Homepage.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './homepage.styles.scss'
+
 const HomePage = () => {
     return (
         <div>
             <h1 className="title">Irimax Development</h1>
         </div>
+        
     );
 };
 
