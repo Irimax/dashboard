c7a7f4fe20bf089b6acbb59d81b0288fbaf92955
lidian <lidianmanoha@gmail.com>
2020-05-24 15:55:07 +0200

    add contact page

---
+++ client_dashboard/src/Container/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './contact.styles.scss';
+
+const Contact = () => {
+  return (
+    <div className='container_contact'>
+      <div className='form'>
+        <input type='email' name='email' id='email' />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
